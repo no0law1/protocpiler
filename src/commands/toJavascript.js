8458366d10f0dca6b27e017793421225165ccfc8
Nuno Reis <nuno.m.reis@netcabo.pt>
2019-01-18 09:05:33 +0000

    Implemented generation of Javascript through proto files

--- src/commands/toJavascript.js
+++ src/commands/toJavascript.js
@@ -1,20 +1,28 @@
-const command = 'javascript [dir]';
+const command = 'javascript [input_dir] [output_dir]';
 
 const describe = 'Generate Javascript for protos';
 
 const builder = { };
 
 const handler = function (argv) {
-    console.log(describe)
-    console.log(argv)
-
-    const { findProtoc } = require('../utils/find')
-
+    const path = require('path');
+    const { findProtoc, findProtos } = require('../utils/find');
     const { execFile } = require('child_process');
 
-    const child = execFile('protoc', ['--version'], { cwd:findProtoc() }, (error, stdout, stderr) => {
+    const protoc = findProtoc();
+
+    process.env["PATH"] += ';' + protoc;
+
+    const options = [
+        '--proto_path=' + path.join(process.cwd(), argv.input_dir),
+        '--js_out=import_style=commonjs:' + path.join(process.cwd(), argv.output_dir)
+    ]
+
+    const protos = findProtos(path.join(process.cwd(), argv.input_dir));
+
+    const child = execFile('protoc', options.concat(protos), { cwd: protoc, env: { PATH: process.env["PATH "]} }, (error, stdout, stderr) => {
         if (error) {
-            console.error(error);
+            console.error('ERROR: ', error);
         }
         console.log(stdout);
     });
--- src/utils/find.js
+++ src/utils/find.js
@@ -13,4 +13,13 @@ const findProtoc = function () {
     }
 }
 
-module.exports = { findProtoc };
+const findProtos = function(input_dir) {
+    const path = require('path');
+    const fs = require('fs');
+    var files = fs.readdirSync(input_dir);
+    return files
+        .filter(file => file.endsWith('.proto'))
+        .map(file => path.join(input_dir, file))
+}
+
+module.exports = { findProtoc, findProtos };
